efb3eb72651ef48cfe314d388fbe55f97c8c49cf
gauravk10-sys <gauravuk10@gmail.com>
2026-08-12 13:24:31 +0530

    Add AgroBiz login module

---
+++ agrobizlogino/src/main/java/com/pravartak/Main.java
@@ -0,0 +1,13 @@
+package com.pravartak;
+
+import com.pravartak.view.login.LoginPage;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Application.launch(LoginPage.class, args);
+    }
+}
