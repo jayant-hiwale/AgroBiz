6d868b41f551f412901deab4bc707c8be2cd7877
vivekB04 <vivek.bhosale2004@gmail.com>
2026-09-01 17:59:31 +0530

    minor change

--- agrobizlogino/src/main/java/com/pravartak/view/farmer/LearningPage.java
+++ agrobizlogino/src/main/java/com/pravartak/view/farmer/LearningPage.java
@@ -9,6 +9,7 @@ import com.pravartak.dao.admindao.FirebaseCourseDAO;
 import com.pravartak.model.admin.Course;
 import com.pravartak.view.farmer.common.Footer;
 import com.pravartak.view.farmer.common.NavBar;
+import com.pravartak.view.login.LoginPage;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -30,13 +31,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.stage.Stage;
 
 public class LearningPage {
-
-    // =========================================================
-    // SCENE
-    // =========================================================
 
     private Scene learningpagScene;
 
@@ -795,11 +791,8 @@ public class LearningPage {
         FarmerModulesPage modulesPage = new FarmerModulesPage(
                 course);
 
-        Stage stage = (Stage) courseContainer
-                .getScene()
-                .getWindow();
 
-        stage.setScene(
+        LoginPage.mainStage.setScene(
                 modulesPage.getModulesPageScene());
     }
 
